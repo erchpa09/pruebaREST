4e08371fd8b745a56c358d8243ee10f97e4e659c
erchpa <2236000543@utna.edu.mx>
2025-06-19 17:11:42 -0600

    Se realiza la conexion de base de datos de mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const CONFIG = require('./app/config/configuracion')
 const app = require('./app/app')
+const conexion = require('./app/config/conexion')
 
+conexion.connect()
 
 app.listen(CONFIG.PORT,()=>{
     console.log('Acplicacion corriendo en puerto', CONFIG.PORT);
